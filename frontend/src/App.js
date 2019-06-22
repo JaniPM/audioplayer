42d7a23212d881694b49fbda1a46f983d02f8ca0
Jani Siivola <jani.siivola@biorna-quantics.com>
2019-06-22 10:24:15 +0100

    Material UI basics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,24 @@ import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme';
 import { Songs } from './pages';
 
 const App = ({ store }) => (
   <Provider store={store}>
     <Router>
-      <div className="content">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route exact path="/" component={Songs} />
-          </Switch>
-        </Suspense>
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="content">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path="/" component={Songs} />
+            </Switch>
+          </Suspense>
+        </div>
+      </ThemeProvider>
     </Router>
   </Provider>
 );
---
+++ frontend/src/theme/index.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "'Montserrat', sans-serif",
+  },
+  shape: {
+    borderRadius: "0"
+  },
+});
+
+export default theme;
